219f75c706e274e1ae63cfd34634fa9e77169f90
chad.myers <chad.myers@gmail.com>
2017-03-25 18:52:49 -0500

    Standardizing  - Remove ripple in favor of nuget  - Remove buildsupport submodule  - Remove docs  - use bundler instead of InstallGems.bat

--- rakefile.rb
+++ rakefile.rb
@@ -1,110 +1,63 @@
-COMPILE_TARGET = ENV['config'].nil? ? "Debug" : ENV['config']
-CLR_TOOLS_VERSION = "v4.0.30319"
-
-buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
-
-if( ! buildsupportfiles.any? )
-  # no buildsupport, let's go get it for them.
-  sh 'git submodule update --init' unless buildsupportfiles.any?
-  buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
-end
-
-# nope, we still don't have buildsupport. Something went wrong.
-raise "Run `git submodule update --init` to populate your buildsupport folder." unless buildsupportfiles.any?
-
-buildsupportfiles.each { |ext| load ext }
-
-include FileTest
-require 'albacore'
-load "VERSION.txt"
-
-RESULTS_DIR = "results"
-PRODUCT = "FubuLocalization"
-COPYRIGHT = 'Copyright 2008-2012 Jeremy D. Miller, Josh Arnold, et al. All rights reserved.';
-COMMON_ASSEMBLY_INFO = 'src/CommonAssemblyInfo.cs';
-
-@teamcity_build_id = "bt396"
+COMPILE_TARGET = ENV['config'].nil? ? "debug" : ENV['config']
+BUILD_VERSION = '100.0.0'
 tc_build_number = ENV["BUILD_NUMBER"]
 build_revision = tc_build_number || Time.new.strftime('5%H%M')
-BUILD_NUMBER = "#{BUILD_VERSION}.#{build_revision}"
-ARTIFACTS = File.expand_path("artifacts")
+build_number = "#{BUILD_VERSION}.#{build_revision}"
+BUILD_NUMBER = build_number 
+msbuild = '"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"'
 
-props = { :stage => File.expand_path("build"), :artifacts => ARTIFACTS }
-
-desc "**Default**, compiles and runs tests"
-task :default => [:compile, :unit_test]
-
-desc "Target used for the CI server"
-task :ci => [:update_all_dependencies, :default, :history, :package]
-
-
+desc 'Compile the code'
+task :compile => [:clean, :version] do
+  sh "#{msbuild} src/FubuLocalization.sln /property:Configuration=#{COMPILE_TARGET} /v:m /t:rebuild /nr:False /maxcpucount:8"
+end
 
 desc "Update the version information for the build"
-assemblyinfo :version do |asm|
-  asm_version = BUILD_VERSION + ".0"
+task :version do
+  asm_version = build_number
   
   begin
     commit = `git log -1 --pretty=format:%H`
   rescue
     commit = "git unavailable"
   end
-  puts "##teamcity[buildNumber '#{BUILD_NUMBER}']" unless tc_build_number.nil?
-  puts "Version: #{BUILD_NUMBER}" if tc_build_number.nil?
-  asm.trademark = commit
-  asm.product_name = PRODUCT
-  asm.description = BUILD_NUMBER
-  asm.version = asm_version
-  asm.file_version = BUILD_NUMBER
-  asm.custom_attributes :AssemblyInformationalVersion => asm_version
-  asm.copyright = COPYRIGHT
-  asm.output_file = COMMON_ASSEMBLY_INFO 
+  puts "##teamcity[buildNumber '#{build_number}']" unless tc_build_number.nil?
+  puts "Version: #{build_number}" if tc_build_number.nil?
+  
+  options = {
+    :description => 'FubuLocalization',
+    :product_name => 'FubuLocalization',
+    :copyright => 'Copyright 2008-2012 Jeremy D. Miller, Josh Arnold, et al. All rights reserved.',
+    :trademark => commit,
+    :version => asm_version,
+    :file_version => build_number,
+    :informational_version => asm_version
+  }
+  
+  puts "Writing src/CommonAssemblyInfo.cs..."
+  File.open('src/CommonAssemblyInfo.cs', 'w') do |file|
+    file.write "using System.Reflection;\n"
+    file.write "using System.Runtime.InteropServices;\n"
+    file.write "[assembly: AssemblyDescription(\"#{options[:description]}\")]\n"
+    file.write "[assembly: AssemblyProduct(\"#{options[:product_name]}\")]\n"
+    file.write "[assembly: AssemblyCopyright(\"#{options[:copyright]}\")]\n"
+    file.write "[assembly: AssemblyTrademark(\"#{options[:trademark]}\")]\n"
+    file.write "[assembly: AssemblyVersion(\"#{options[:version]}\")]\n"
+    file.write "[assembly: AssemblyFileVersion(\"#{options[:file_version]}\")]\n"
+    file.write "[assembly: AssemblyInformationalVersion(\"#{options[:informational_version]}\")]\n"
+  end
 end
 
 desc "Prepares the working directory for a new build"
-task :clean => [:update_buildsupport] do
-	#TODO: do any other tasks required to clean/prepare the working directory
-	FileUtils.rm_rf props[:stage]
-    # work around nasty latency issue where folder still exists for a short while after it is removed
-    waitfor { !exists?(props[:stage]) }
-	Dir.mkdir props[:stage]
-	
-	FileUtils.rm_rf props[:artifacts]
-    # work around nasty latency issue where folder still exists for a short while after it is removed
-    waitfor { !exists?(props[:artifacts]) }
-	Dir.mkdir props[:artifacts]
-    
-	Dir.mkdir props[:artifacts] unless exists?(props[:artifacts])
+task :clean do
+  FileUtils.rm_rf 'artifacts'
+  Dir.mkdir 'artifacts'
 end
 
-def waitfor(&block)
-  checks = 0
-  until block.call || checks >10 
-    sleep 0.5
-    checks += 1
-  end
-  raise 'waitfor timeout expired' if checks > 10
+desc 'Run the unit tests'
+task :test => [:compile, :fast_test] do
 end
 
-
-desc "Compiles the app"
-task :compile => [:clean, :restore_if_missing, :version, "docs:bottle"] do
-  MSBuildRunner.compile :compilemode => COMPILE_TARGET, :solutionfile => 'src/FubuLocalization.sln', :clrversion => CLR_TOOLS_VERSION
-  copyOutputFiles "src/FubuLocalization/bin/#{COMPILE_TARGET}", "FubuLocalization*.{dll,pdb}", props[:stage]
-  copyOutputFiles "src/localizer/bin/#{COMPILE_TARGET}", "localizer*.{exe,pdb}", props[:stage]   
-  copyOutputFiles "src/localizer/bin/#{COMPILE_TARGET}", "FubuCore.dll", props[:stage]   
-end
-
-def copyOutputFiles(fromDir, filePattern, outDir)
-  Dir.glob(File.join(fromDir, filePattern)){|file| 		
-	copy(file, outDir) if File.file?(file)
-  } 
-end
-
-desc "Runs unit tests"
-task :test => [:unit_test]
-
-desc "Runs unit tests"
-task :unit_test => :compile do
-  runner = NUnitRunner.new :compilemode => COMPILE_TARGET, :source => 'src', :platform => 'x86'
-  runner.executeTests ['FubuLocalization.Tests']
+desc 'Run the unit tests without compile'
+task :fast_test do
+  sh "src/packages/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe src/FubuLocalization.Tests/bin/#{COMPILE_TARGET}/FubuLocalization.Tests.dll"
 end
